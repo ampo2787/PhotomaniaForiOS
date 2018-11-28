dafc3d92e2fcc126a8959090083a17b6a715db35
JIHOON PARK <ampo2787@cnu.ac.kr>
2018-11-28 10:10:32 +0900

    Initial Commit

---
+++ PhotomaniaForiOS/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  PhotomaniaForiOS
+//
+//  Created by JihoonPark on 28/11/2018.
+//  Copyright © 2018 JihoonPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ PhotomaniaForiOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhotomaniaForiOS
+//
+//  Created by JihoonPark on 28/11/2018.
+//  Copyright © 2018 JihoonPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
